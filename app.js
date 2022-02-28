86d502b9f1dcdb523c86004fe3a6e3994c1ba46e
rahad2694 <rahad.haw@gmail.com>
2022-02-28 12:29:54 +0600

    Error Handled

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
 const searchResultContainer = document.getElementById('result-container');
-
+const errorMsg = document.getElementById('error-msg');
 const searchBtn =() =>{
     const searchInputBox = document.getElementById('search-input-box');
     const url = `https://www.themealdb.com/api/json/v1/1/search.php?s=${searchInputBox.value}`;
-    console.log(url);
+    // console.log(url);
     fetch(url)
     .then(res => res.json())
     .then(data => showMeals(data.meals))
@@ -14,19 +14,25 @@ const searchBtn =() =>{
 const showMeals = (meals) =>{
     // console.log(meals);
     searchResultContainer.innerHTML='';
-    meals.forEach( meal =>{
-        console.log(meal);
-        const div = document.createElement('div');
-        div.classList.add('col');
-        div.innerHTML=`
-        <div class="card h-100">
-            <img class="img-fluid" src="${meal.strMealThumb}" class="card-img-top" alt="...">
-            <div class="card-body">
-                <h5 class="card-title">${meal.strMeal}</h5>
-                <p class="card-text">${meal.strInstructions.slice(0,200)}</p>
+    if(meals == null){
+        errorMsg.innerText='No match Found!';
+    }
+    else{
+        errorMsg.innerText='';
+        meals.forEach( meal =>{
+            // console.log(meal);
+            const div = document.createElement('div');
+            div.classList.add('col');
+            div.innerHTML=`
+            <div class="card h-100">
+                <img class="img-fluid" src="${meal.strMealThumb}" class="card-img-top" alt="...">
+                <div class="card-body">
+                    <h5 class="card-title">${meal.strMeal}</h5>
+                    <p class="card-text">${meal.strInstructions.slice(0,200)}</p>
+                </div>
             </div>
-        </div>
-        `;
-        searchResultContainer.appendChild(div);
-    });
+            `;
+            searchResultContainer.appendChild(div);
+        });
+    }
 }
